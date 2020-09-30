99701ddae27df50f17244bc64600a8f482d4b8e8
Rafael Rondao <rafael.rondao@gmail.com>
2020-09-30 19:18:00 +0000

    Refactore code into functions
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- rondao_env.sh
+++ rondao_env.sh
@@ -59,87 +59,114 @@ command_exists() {
   command -v "$@" >/dev/null 2>&1
 }
 
-print_title
-
-
-print_task "Apply GNOME settings"
-if command_exists gnome-shell; then
-  if command -v dconf > /dev/null 2>&1; then
-    if test -f dconf-gnome.ini; then
-      dconf load / < dconf-gnome.ini
-      print_ok "GNOME settings applied."
+configure_gnome_settings() {
+  print_task "Configure GNOME settings"
+  if command_exists gnome-shell; then
+    if command -v dconf > /dev/null 2>&1; then
+      if test -f dconf-gnome.ini; then
+        dconf load / < dconf-gnome.ini
+        print_ok "GNOME settings applied."
+      else
+        print_fail "dconf-gnome.ini file not found."
+        print_in_red "Skip GNOME settings."
+      fi
     else
-      print_fail "dconf-gnome.ini file not found."
+      print_fail "dconf command not found."
       print_in_red "Skip GNOME settings."
     fi
   else
-    print_fail "dconf command not found."
+    print_fail "gnome-shell command not found."
     print_in_red "Skip GNOME settings."
   fi
-else
-  print_fail "gnome-shell command not found."
-  print_in_red "Skip GNOME settings."
-fi
+}
 
-print_task "Install Vim"
-sudo apt -y install vim
-print_ok "Vim installed."
+install_vim() {
+  print_task "Install Vim"
+  sudo apt -y install vim
+  print_ok "Vim installed."
+}
 
-print_task "Apply Git identity"
-if command_exists git; then
-  git config --global user.email "rafael.rondao@gmail.com"
-  git config --global user.name "Rafael Rondao"
-  print_ok "Git identity applied."
-else
-  print_fail "Git is not installed."
-fi
-
-print_task "Configure Git default editor to Vim"
-git config --global core.editor vim
-print_ok "Git default editor is now Vim."
-
-print_task "Install Zsh"
-if ! command_exists zsh; then
-  sudo apt -y install zsh
-  if command_exists zsh; then
-    print_ok "ZSH was installed."
+configure_git() {
+  print_task "Apply Git identity"
+  if command_exists git; then
+    git config --global user.email "rafael.rondao@gmail.com"
+    git config --global user.name "Rafael Rondao"
+    print_ok "Git identity applied."
   else
-    print_fail "ZSH failed to install."
+    print_fail "Git is not installed."
   fi
-else
-  print_ok "ZSH is already installed."
-fi
 
-print_task "Install OhMyZsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
-print_ok "OhMyZsh was installed."
+  print_task "Configure Git default editor to Vim"
+  git config --global core.editor vim
+  print_ok "Git default editor is now Vim."
+}
 
-print_task "Copy OhMyZsh config file"
-cp .zshrc $HOME/
-print_ok "OhMyZsh config file copied."
+install_zsh() {
+  print_task "Install Zsh"
+  if ! command_exists zsh; then
+    sudo apt -y install zsh
+    if command_exists zsh; then
+      print_ok "ZSH was installed."
+    else
+      print_fail "ZSH failed to install."
+    fi
+  else
+    print_ok "ZSH is already installed."
+  fi
+}
 
-print_task "Install Meslo Nerd Font"
-wget -P ~/.fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-wget -P ~/.fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-wget -P ~/.fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-wget -P ~/.fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
-print_ok "Meslo Nerd Font installed."
+install_and_configure_oh_my_zsh() {
+  print_task "Install OhMyZsh"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
+  print_ok "OhMyZsh was installed."
 
-print_task "Install PowerLevel10K theme for OhMyZsh"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-print_ok "PowerLevel10K theme installed."
+  print_task "Copy OhMyZsh config file"
+  cp .zshrc $HOME/
+  print_ok "OhMyZsh config file copied."
+}
 
-print_task "Copy PowerLevel10K config file"
-cp .p10k.zsh $HOME/
-print_ok "PowerLevel10K config file copied."
+install_meslo_font() {
+  print_task "Install Meslo Nerd Font"
+  wget -P ~/.fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+  wget -P ~/.fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+  wget -P ~/.fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+  wget -P ~/.fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+  print_ok "Meslo Nerd Font installed."
+}
 
+install_and_configure_powerlevel10k() {
+  print_task "Install PowerLevel10K theme for OhMyZsh"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+  print_ok "PowerLevel10K theme installed."
 
-print_task "Change default shell to Zsh."
-if ! chsh -s command -v zsh; then
-  print_fail "chsh command unsuccessful. Change your default shell manually."
-else
-  export SHELL=`command -v zsh`
-  print_ok "Shell successfully changed to Zsh."
-fi
+  print_task "Copy PowerLevel10K config file"
+  cp .p10k.zsh $HOME/
+  print_ok "PowerLevel10K config file copied."
+}
 
-print_warn "You need to change your Terminal font to Meslo manually."
+configure_zsh_as_default_shell() {
+  print_task "Change default shell to Zsh."
+  if ! chsh -s command -v zsh; then
+    print_fail "chsh command unsuccessful. Change your default shell manually."
+  else
+    export SHELL=`command -v zsh`
+    print_ok "Shell successfully changed to Zsh."
+  fi
+}
+
+main() {
+  print_title
+
+  configure_gnome_settings
+  install_vim
+  configure_git
+  install_zsh
+  install_and_configure_oh_my_zsh
+  install_meslo_font
+  install_and_configure_powerlevel10k
+  configure_zsh_as_default_shell
+
+  print_warn "You need to change your Terminal font to Meslo manually."
+}
+
+main "$@"
