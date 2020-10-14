f1c50b195c1d56c1f1c079d8d80f95b643901bed
Rafael Rondao <rafael.rondao@gmail.com>
2020-10-13 21:43:22 -0300

    Install Docker and Kubernetes
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- rondao_env.sh
+++ rondao_env.sh
@@ -261,6 +261,34 @@ restart_gnome_shell() {
   killall -SIGQUIT gnome-shell
 }
 
+install_docker() {
+  sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+  sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+  sudo get update
+  sudo get install docker-ce docker-ce-cli containerd.io
+}
+
+install_kubernetes_with_minikube() {
+  curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+  chmod +x ./kubectl
+  sudo mv ./kubectl /usr/local/bin/kubectl
+
+  curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+  sudo install minikube-linux-amd64 /usr/local/bin/minikube
+}
+
 install_flutter() {
   local INSTALL=$HOME/apps
 
@@ -359,6 +387,9 @@ main() {
   if [ -v INTERACTIVE ]; then
     install_flatpak_apps_list
   fi
+
+  install_docker
+  install_kubernetes_with_minikube
 
   install_flutter
   install_nodejs
